a77f7396e360e3f62ec4c18666b43832e2e4910c
d4ptak <d4ptak@gmail.com>
2020-10-18 19:36:15 +0200

    Init project structure

---
+++ src/main/java/com/d4ptak/springdatajpatestcontainers/SpringDataJpaTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package com.d4ptak.springdatajpatestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaTestcontainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaTestcontainersApplication.class, args);
+	}
+
+}
